5401c5a82aac7bec16d05adb997d9cd582191674
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-22 16:43:23 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,11 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/name' do
-    "My name is Breyanna."
+    "My name is Breyanna"
   end
 
   get '/hometown' do
-
+    "My hometown is Boston, MA,"
   end
 
   get '/favorite-song' do
