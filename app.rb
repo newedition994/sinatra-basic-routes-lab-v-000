decf823e4cf03b26ac674d499416358b03b04064
Breyanna Vass <breyanna.bvass@gmail.com>
2017-12-22 16:44:34 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Breyanna"
+  end
+
+  get '/hometown' do
+    "My hometown is Boston, MA,"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'Watch Me' by Jaden Smith."
+  end
 end
